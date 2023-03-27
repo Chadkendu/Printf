358be9a2fc18111b5fb693781265a2df4ba4602a
Joshua <kenduajayi@gmail.com>
2023-03-27 21:14:00 +0100

    OmSll: Add print functions for char, string, percent, int, and binary

--- functions.c
+++ functions.c
@@ -1,4 +1,7 @@
 #include "main.h"
+
+/************************* PRINT CHAR *************************/
+
 /**
  * print_char - Prints a char
  * @types: List a of arguments
@@ -12,11 +15,11 @@
 int print_char(va_list types, char buffer[],
 	int flags, int width, int precision, int size)
 {
-	char _c = va_arg(types, int);
+	char c = va_arg(types, int);
 
-	return (handle_write_char(_c, buffer, flags, width, precision, size));
+	return (handle_write_char(c, buffer, flags, width, precision, size));
 }
-/*String Func*/
+/************************* PRINT A STRING *************************/
 /**
  * print_string - Prints a string
  * @types: List a of arguments
@@ -30,47 +33,48 @@ int print_char(va_list types, char buffer[],
 int print_string(va_list types, char buffer[],
 	int flags, int width, int precision, int size)
 {
-	int index, l = 0;
-	char *p = va_arg(types, char *);
+	int length = 0, i;
+	char *str = va_arg(types, char *);
 
 	UNUSED(buffer);
 	UNUSED(flags);
 	UNUSED(width);
 	UNUSED(precision);
 	UNUSED(size);
-	if (p == NULL)
+	if (str == NULL)
 	{
-		p = "(null)";
+		str = "(null)";
 		if (precision >= 6)
-			p = "      ";
+			str = "      ";
 	}
-	while (p[l] != '\0')
-		l++;
-	if (precision >= 0 && precision < l)
-		l = precision;
-	if (width > l)
+
+	while (str[length] != '\0')
+		length++;
+
+	if (precision >= 0 && precision < length)
+		length = precision;
+
+	if (width > length)
 	{
 		if (flags & F_MINUS)
 		{
-			write(1, &p[0], l);
-			for (index = width - l; index > 0; index--)
+			write(1, &str[0], length);
+			for (i = width - length; i > 0; i--)
 				write(1, " ", 1);
 			return (width);
 		}
 		else
 		{
-			for (index = width - l; index > 0; index--)
-			{
+			for (i = width - length; i > 0; i--)
 				write(1, " ", 1);
-				index--;
-			}
-			write(1, &p[0], l);
+			write(1, &str[0], length);
 			return (width);
 		}
 	}
-	return (write(1, p, l));
+
+	return (write(1, str, length));
 }
-/*Percent Func*/
+/************************* PRINT PERCENT SIGN *************************/
 /**
  * print_percent - Prints a percent sign
  * @types: Lista of arguments
@@ -93,9 +97,7 @@ int print_percent(va_list types, char buffer[],
 	return (write(1, "%%", 1));
 }
 
-
-
-
+/************************* PRINT INT *************************/
 /**
  * print_int - Print int
  * @types: Lista of arguments
@@ -109,7 +111,7 @@ int print_percent(va_list types, char buffer[],
 int print_int(va_list types, char buffer[],
 	int flags, int width, int precision, int size)
 {
-	int index = BUFF_SIZE - 2;
+	int i = BUFF_SIZE - 2;
 	int is_negative = 0;
 	long int n = va_arg(types, long int);
 	unsigned long int num;
@@ -117,30 +119,29 @@ int print_int(va_list types, char buffer[],
 	n = convert_size_number(n, size);
 
 	if (n == 0)
-	{
-		buffer[index--] = '0';
-	}
+		buffer[i--] = '0';
+
 	buffer[BUFF_SIZE - 1] = '\0';
 	num = (unsigned long int)n;
+
 	if (n < 0)
 	{
 		num = (unsigned long int)((-1) * n);
 		is_negative = 1;
 	}
+
 	while (num > 0)
 	{
-		buffer[index--] = (num % 10) + '0';
+		buffer[i--] = (num % 10) + '0';
 		num /= 10;
 	}
-	index++;
-	return (write_number(is_negative, index,
-			buffer, flags, width, precision, size));
+
+	i++;
+
+	return (write_number(is_negative, i, buffer, flags, width, precision, size));
 }
 
-
-
-
-
+/************************* PRINT BINARY *************************/
 /**
  * print_binary - Prints an unsigned number
  * @types: Lista of arguments
@@ -154,7 +155,7 @@ int print_int(va_list types, char buffer[],
 int print_binary(va_list types, char buffer[],
 	int flags, int width, int precision, int size)
 {
-	unsigned int n, m, index, sum;
+	unsigned int n, m, i, sum;
 	unsigned int a[32];
 	int count;
 
@@ -165,21 +166,19 @@ int print_binary(va_list types, char buffer[],
 	UNUSED(size);
 
 	n = va_arg(types, unsigned int);
-	m = 2147483648;
+	m = 2147483648; /* (2 ^ 31) */
 	a[0] = n / m;
-	index = 1;
-	while (index < 32)
+	for (i = 1; i < 32; i++)
 	{
 		m /= 2;
-		a[index] = (n / m) % 2;
-		index++;
+		a[i] = (n / m) % 2;
 	}
-	for (index = 0, sum = 0, count = 0; index < 32; index++)
+	for (i = 0, sum = 0, count = 0; i < 32; i++)
 	{
-		sum += a[index];
-		if (sum || index == 31)
+		sum += a[i];
+		if (sum || i == 31)
 		{
-			char z = '0' + a[index];
+			char z = '0' + a[i];
 
 			write(1, &z, 1);
 			count++;
