8c808d4fac3763c57ac4585d50c93b359d98f44a
Oumaima SLL <58479472+emaoumaima@users.noreply.github.com>
2023-03-27 22:10:32 +0200

    Parv: printf (main) & printf_buffer function

--- _printf.c
+++ _printf.c
@@ -1,71 +1,66 @@
 #include "main.h"
-/**
- * printf_buffer - func
- * @buffer: array
- * @buff_ind: index
- */
+
 void print_buffer(char buffer[], int *buff_ind);
+
 /**
  * _printf - Printf function
- * @format: pointer.
+ * @format: format.
  * Return: Printed chars.
  */
 int _printf(const char *format, ...)
 {
-	int printed = 0, printed_chars = 0, buffer_index = 0;
-	int index, flag, w, s, precision;
-	va_list va;
+	int i, printed = 0, printed_chars = 0;
+	int flags, width, precision, size, buff_ind = 0;
+	va_list list;
 	char buffer[BUFF_SIZE];
 
 	if (format == NULL)
-	{
 		return (-1);
-	}
-	va_start(va, format);
-	index = 0;
-	while (format && format[index] != '\0')
+
+	va_start(list, format);
+
+	for (i = 0; format && format[i] != '\0'; i++)
 	{
-		if (format[index] != '%')
+		if (format[i] != '%')
 		{
-			buffer[buffer_index++] = format[index];
-			if (buffer_index == BUFF_SIZE)
-			{
-				print_buffer(buffer, &buffer_index);
-			}
+			buffer[buff_ind++] = format[i];
+			if (buff_ind == BUFF_SIZE)
+				print_buffer(buffer, &buff_ind);
+			/* write(1, &format[i], 1);*/
 			printed_chars++;
 		}
 		else
 		{
-			print_buffer(buffer, &buffer_index);
-			flag = get_flags(format, &index);
-			w = get_width(format, &index, va);
-			precision = get_precision(format, &index, va);
-			s = get_size(format, &index);
-			++index;
-			printed = handle_print(format, &index, va, buffer,
-				flag, w, precision, s);
+			print_buffer(buffer, &buff_ind);
+			flags = get_flags(format, &i);
+			width = get_width(format, &i, list);
+			precision = get_precision(format, &i, list);
+			size = get_size(format, &i);
+			++i;
+			printed = handle_print(format, &i, list, buffer,
+				flags, width, precision, size);
 			if (printed == -1)
-			{
 				return (-1);
-			}
 			printed_chars += printed;
 		}
-		index++;
 	}
-	print_buffer(buffer, &buffer_index);
-	va_end(va);
+
+	print_buffer(buffer, &buff_ind);
+
+	va_end(list);
+
 	return (printed_chars);
 }
+
 /**
- * print_buffer - Prints function
- * @buffer: Array
- * @buff_ind: var
+ * print_buffer - Prints the contents of the buffer if it exist
+ * @buffer: Array of chars
+ * @buff_ind: Index at which to add next char, represents the length.
  */
 void print_buffer(char buffer[], int *buff_ind)
 {
 	if (*buff_ind > 0)
-	{
 		write(1, &buffer[0], *buff_ind);
-	}
+
 	*buff_ind = 0;
 }
